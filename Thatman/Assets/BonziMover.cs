d35ec95ce9efd3bad4874ccbd9a973ca19a2a062
MCK144 <matt.kraehemann@gmail.com>
2021-11-12 20:23:34 -0600

    Added a bonzi buddy character

---
+++ Thatman/Assets/BonziMover.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonziMover : MonoBehaviour
+{
+    public Transform trans;
+    public Rigidbody2D rb;
+    float x;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        x = trans.position.x;
+
+        Debug.Log(x.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 force = new Vector3(6, 0, 0);
+        rb.AddForce(0,1,0);
+
+    }
+}
+
+/* if (Input.GetKey("a") == true)
+        {
+            Vector3 newPos = new Vector3(9, 0, 0);
+            transform.position.x = 0;
+            //rb.AddForce(-1.0f,0.0f,0.0f);
+        }
+        if (Input.GetKey("d") == true)
+        {
+            //rb.AddForce(1.0f,0.0f,0.0f);
+        }*/
